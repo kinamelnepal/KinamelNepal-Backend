617684c3f19cc814656671e7c4885a27e5002a5a
Surya Giri <surya.giri@inholland.nl>
2025-05-04 15:29:01 +0200

    CI/CD created

---
+++ passenger_wsgi.py
@@ -0,0 +1,7 @@
+import sys, os
+
+sys.path.insert(0, os.path.dirname(__file__))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'core.settings'
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
